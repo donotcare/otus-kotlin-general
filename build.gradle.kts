378af7f674804f146f67ee545ce514857ce5dd7c
Zhmylev Alexey <zhmylevaa@gmail.com>
2021-01-12 21:23:45 +0300

    add first test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
+    testImplementation("org.assertj:assertj-core:3.18.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+    }
 }
---
+++ src/test/kotlin/ru/otus/general/MainKtTest.kt
@@ -0,0 +1,11 @@
+package ru.otus.general
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+    @Test
+    fun things_should_work() {
+        assertThat(1).isEqualTo(1)
+    }
+}
