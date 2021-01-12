32d09c0099a61caa696c5187a61211ae9be613d7
Zhmylev Alexey <zhmylevaa@gmail.com>
2021-01-12 20:52:16 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "ru.otus"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
